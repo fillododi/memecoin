fb1c03c7ceefce60025c9f2fe3a3964fed0823a0
fillododi <fillo.dodi@gmail.com>
2025-01-23 16:40:32 +0100

    rough start

--- programs/memecoin/src/lib.rs
+++ programs/memecoin/src/lib.rs
@@ -1,4 +1,14 @@
 use anchor_lang::prelude::*;
+use anchor_spl::token::{
+    self,
+    Mint,
+    Token,
+    TokenAccount,
+    MintTo,
+    InitializeMint,
+    InitializeAccount,
+    Transfer
+}
 
 declare_id!("Hf6mGsNZNcGxTVwmRVRbzZsAL2ezW7NNMxcivRLssE6X");
 
@@ -6,11 +16,21 @@ declare_id!("Hf6mGsNZNcGxTVwmRVRbzZsAL2ezW7NNMxcivRLssE6X");
 pub mod memecoin {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
+    //initializes the mint and mints some tokens
+    pub fn initialize_mint(ctx: Context<InitializeMintAccounts>, bump: u8, decimals: u8, amount: u64) -> Result<()> {
+        //initializing mint
+        let cpi_accounts = token::InitializeMint {
+            mint: ctx.accounts.mint.to_account_info(),
+            rent: ctx.accounts.rent.to_account_info()
+        };
+        let cpi_program = ctx.accounts.token_program.to_account_info();
+        let init_mint_ctx = CpiContext::new(cpi_program, cpi_accounts);
+        token::initialize_mint(init_mint_ctx, decimals, ctx.accounts.authority.key, None)?;
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct InitializeMintAccounts<'info> {
+
+}
